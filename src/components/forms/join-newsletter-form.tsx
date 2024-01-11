a0dd5dc5f67ba90fbe1319448a5a3221cc6f5685
Kinfe123 <kinfishtech@gmail.com>
2024-01-11 21:43:37 +0300

    fix: typo fix

--- src/components/forms/join-newsletter-form.tsx
+++ src/components/forms/join-newsletter-form.tsx
@@ -85,7 +85,7 @@ export function JoinNewsletterForm() {
               <FormLabel className="sr-only">Email</FormLabel>
               <FormControl>
                 <Input
-                  placeholder="skate@gmail.com"
+                  placeholder="melaku@gmail.com"
                   className="pr-12"
                   {...field}
                 />
