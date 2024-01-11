ac7c1de4a3f94655fb7bba43f18b44c809949eee
Kinfe123 <kinfishtech@gmail.com>
2024-01-11 10:17:31 +0300

    fix: spacing fix

--- src/config/site.ts
+++ src/config/site.ts
@@ -76,7 +76,7 @@ export const siteConfig = {
         },
         {
           title: "Contact",
-          href: "/pages/Contact",
+          href: "/contact",
           external: false,
         },
         {
