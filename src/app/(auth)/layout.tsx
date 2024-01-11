e29100f7a42d5c173c879266eee4fcb01088374c
Kinfe123 <kinfishtech@gmail.com>
2024-01-11 09:40:57 +0300

    pic added

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -4,13 +4,14 @@ import Link from "next/link"
 import { siteConfig } from "@/config/site"
 import { AspectRatio } from "@/components/ui/aspect-ratio"
 import { Icons } from "@/components/icons"
+import CardImage from '../../../public/labpic.jpg'
 
 export default function AuthLayout({ children }: React.PropsWithChildren) {
   return (
     <div className="grid min-h-screen grid-cols-1 overflow-hidden md:grid-cols-3 lg:grid-cols-2">
       <AspectRatio ratio={16 / 9}>
         <Image
-          src="/images/auth-layout.webp"
+          src="/labpic2.jpg"
           alt="A skateboarder doing a high drop"
           fill
           className="absolute inset-0 object-cover"
@@ -27,19 +28,19 @@ export default function AuthLayout({ children }: React.PropsWithChildren) {
         </Link>
         <div className="absolute bottom-6 left-8 z-20 line-clamp-1 text-base">
           Photo by{" "}
-          <a
+          {/* <a
             href="https://unsplash.com/ja/@pixelperfektion?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
             className="hover:underline"
           >
             pixelperfektion
-          </a>
+          </a> */}
           {" on "}
-          <a
+          {/* <a
             href="https://unsplash.com/photos/OS2WODdxy1A?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
             className="hover:underline"
           >
             Unsplash
-          </a>
+          </a> */}
         </div>
       </AspectRatio>
       <main className="container absolute top-1/2 col-span-1 flex -translate-y-1/2 items-center md:static md:top-0 md:col-span-2 md:flex md:translate-y-0 lg:col-span-1">
